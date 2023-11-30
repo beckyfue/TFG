932f3d782424c785fedf45ce9279ad2692384bf5
beckyfue <r.fuentesa.2020@alumnnos.urjc.es>
2023-11-30 16:47:51 +0100

    reset button (without ckeck if email in database)

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
 app_name = "polls"
 
 urlpatterns = [
@@ -17,6 +19,11 @@ urlpatterns = [
     path('games/', views.games, name='games'),
     path('patient_homepage/', views.patient_homepage, name='patient_homepage'),
     path('patient_detail/<int:patient_id>/', views.patient_detail, name='patient_detail'),
+
+    path('password_reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 
 
