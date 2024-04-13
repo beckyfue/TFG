042f0f1f6467a8a642e0b9770fcfc10f9c2ce790
unknown <davidrol6@hotmail.com>
2024-04-13 15:19:00 +0200

    Minor tweak

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-SERVER = "127.0.0.1"
+SERVER = "http://127.0.0.1"
 PORT = 8000
 
 AUTH_USER_MODEL = 'polls.CustomUser'
